2c269138e4e3532f1d860ea42a2984f3512a8faa
futureimperfect <james@everythingisgray.com>
2015-01-12 18:00:06 -0800

    Upgrade optparse code to argparse.

--- osxcollector/output_filters/analyze.py
+++ osxcollector/output_filters/analyze.py
@@ -24,8 +24,8 @@
 # 12. Party!
 #
 import sys
+from argparse import ArgumentParser
 from numbers import Number
-from optparse import OptionParser
 
 import simplejson
 from osxcollector.output_filters.base_filters.chain import ChainFilter
@@ -399,40 +399,40 @@ class _VeryReadableOutputFilter(OutputFilter):
 
 
 def main():
-    parser = OptionParser(usage='usage: %prog [options]')
-    parser.add_option('-f', '--file-term', dest='file_terms', default=[], action='append',
-                      help='[OPTIONAL] Suspicious terms to use in pivoting through file names.  May be specified more than once.')
-    parser.add_option('-d', '--domain', dest='domain_terms', default=[], action='append',
-                      help='[OPTIONAL] Suspicious domains to use for pivoting.  May be specified more than once.')
-    parser.add_option('-i', '--ip', dest='ip_terms', default=[], action='append',
-                      help='[OPTIONAL] Suspicious IP to use for pivoting.  May be specified more than once.')
-    parser.add_option('--related-domains-generations', dest='related_domains_generations', default=DEFAULT_RELATED_DOMAINS_GENERATIONS,
-                      help='[OPTIONAL] How many generations of related domains to lookup with OpenDNS')
-    parser.add_option('--readout', dest='readout', action='store_true', default=False,
-                      help='[OPTIONAL] Skip the analysis and just output really readable analysis')
-    parser.add_option('-M', '--monochrome', dest='monochrome', action='store_true', default=False,
-                      help='[OPTIONAL] Output monochrome analysis')
-    parser.add_option('--no-opendns', dest='no_opendns', action='store_true', default=False,
-                      help='[OPTIONAL] Don\'t run OpenDNS filters')
-    parser.add_option('--no-virustotal', dest='no_virustotal', action='store_true', default=False,
-                      help='[OPTIONAL] Don\'t run VirusTotal filters')
-    parser.add_option('--no-shadowserver', dest='no_shadowserver', action='store_true', default=False,
-                      help='[OPTIONAL] Don\'t run ShadowServer filters')
-    parser.add_option('--input-file', dest='input_file', default=None,
-                      help='[OPTIONAL] Path to OSXCollector output to read. Defaults to stdin otherwise.')
+    parser = ArgumentParser()
+    parser.add_argument('-f', '--file-term', dest='file_terms', default=[], action='append',
+                        help='[OPTIONAL] Suspicious terms to use in pivoting through file names.  May be specified more than once.')
+    parser.add_argument('-d', '--domain', dest='domain_terms', default=[], action='append',
+                        help='[OPTIONAL] Suspicious domains to use for pivoting.  May be specified more than once.')
+    parser.add_argument('-i', '--ip', dest='ip_terms', default=[], action='append',
+                        help='[OPTIONAL] Suspicious IP to use for pivoting.  May be specified more than once.')
+    parser.add_argument('--related-domains-generations', dest='related_domains_generations', default=DEFAULT_RELATED_DOMAINS_GENERATIONS,
+                        help='[OPTIONAL] How many generations of related domains to lookup with OpenDNS')
+    parser.add_argument('--readout', dest='readout', action='store_true', default=False,
+                        help='[OPTIONAL] Skip the analysis and just output really readable analysis')
+    parser.add_argument('-M', '--monochrome', dest='monochrome', action='store_true', default=False,
+                        help='[OPTIONAL] Output monochrome analysis')
+    parser.add_argument('--no-opendns', dest='no_opendns', action='store_true', default=False,
+                        help='[OPTIONAL] Don\'t run OpenDNS filters')
+    parser.add_argument('--no-virustotal', dest='no_virustotal', action='store_true', default=False,
+                        help='[OPTIONAL] Don\'t run VirusTotal filters')
+    parser.add_argument('--no-shadowserver', dest='no_shadowserver', action='store_true', default=False,
+                        help='[OPTIONAL] Don\'t run ShadowServer filters')
+    parser.add_argument('--input-file', dest='input_file', default=None,
+                        help='[OPTIONAL] Path to OSXCollector output to read. Defaults to stdin otherwise.')
 
-    options, __ = parser.parse_args()
+    args = parser.parse_args()
 
-    if not options.readout:
-        output_filter = AnalyzeFilter(initial_file_terms=options.file_terms, initial_domains=options.domain_terms,
-                                      initial_ips=options.ip_terms, related_domains_generations=options.related_domains_generations,
-                                      monochrome=options.monochrome, no_opendns=options.no_opendns, no_virustotal=options.no_virustotal,
-                                      no_shadowserver=options.no_shadowserver)
+    if not args.readout:
+        output_filter = AnalyzeFilter(initial_file_terms=args.file_terms, initial_domains=args.domain_terms,
+                                      initial_ips=args.ip_terms, related_domains_generations=args.related_domains_generations,
+                                      monochrome=args.monochrome, no_opendns=args.no_opendns, no_virustotal=args.no_virustotal,
+                                      no_shadowserver=args.no_shadowserver)
     else:
-        output_filter = _VeryReadableOutputFilter(monochrome=options.monochrome)
+        output_filter = _VeryReadableOutputFilter(monochrome=args.monochrome)
 
-    if options.input_file:
-        with(open(options.input_file, 'r')) as fp_in:
+    if args.input_file:
+        with(open(args.input_file, 'r')) as fp_in:
             run_filter(output_filter, input_stream=fp_in)
     else:
         run_filter(output_filter)
